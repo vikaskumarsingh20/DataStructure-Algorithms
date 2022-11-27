c688c472e59d5c9567a4db4e50d01ac1b98d8f5d
Vikas Kumar Singh <92667215+vikasrajput01@users.noreply.github.com>
2022-11-27 10:43:53 +0530

    Update minCostClimbingStairs.cpp

--- DataStructure-Algorithms/DynamicProgramming/minCostClimbingStairs.cpp
+++ DataStructure-Algorithms/DynamicProgramming/minCostClimbingStairs.cpp
@@ -29,27 +29,69 @@ Explanation: You will start at index 0.
 The total cost is 6.
 
 SOLUTION FOR IT GIVEN BELOW 
-
-class Solution {
+class Solution
+{
 public:
-    
-    int solve(vector<int > &cost , int n, vector<int> &dp){
-        // base case
-        if( n == 0 ) return cost[0];
-        if(n == 1)  return cost[1];
-        
-         if(dp[n] != -1)
-            return dp[n];  // if alredy presented then return array
-        
-   dp[n] = cost[n] + min (solve(cost , n -1 ,dp) , solve(cost , n -2 , dp)); /// find mini then add with previous array
-        return dp[n];
+
+// TIME COMPLEXITY EXPONTIAL SPACE ALSO
+
+int solveRecursion(vector<int > &cost , int n){
+    // base case
+    if(n == 0 || n == 1)
+      return n;
+
+      return (cost[n] + min(solveRecursion(n-1) , solveRecursion(n-2)));
+}
+
+// TIME COMPLEXITY O(N) WITH SPACE O(N) + O(N)
+    int solveRecMemo(vector<int> &cost, int n)
+    {
+        //   base case
+                if( n == 0 ) return cost[0];
+                if(n == 1)  return cost[1];
+
+                 if(dp[n] != -1)
+                    return dp[n];
+
+           dp[n] = cost[n] + min (solve(cost , n -1 ,dp) , solve(cost , n -2 , dp));
+                return dp[n];
     }
-    
-    int minCostClimbingStairs(vector<int>& cost) {
-        
+
+    // TIME COMPLEXITY O(N) WITH SPACE O(N)
+    int solveTabulation(vector<int >& cost , int n , vector<int > &dp){
+
+        // base case 
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int  i = 2;i<n;i++){
+            dp[i] = cost[i] + min(dp[n-1] , dp[n-2]);
+        }
+
+        return min(dp[n-1]  , dp[n-2]);
+    }
+      // SPACE TOPIMAZATION O(1) WITH O(N) TIME COMPLEXITY
+    int solveSapceOptm(vector<int> &cost  , int n){
+        int prev0 = cost[0];
+        int prev1 = cost[1];
+
+        for (int i = 2; i < n; i++)
+        {
+            int curr = cost[i] + min(prev0, prev1);
+            prev0 = prev1;
+            prev1 = curr;
+        }
+        return min(prev0, prev1);
+    }
+
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+
+        // int n = cost.size();
+        // vector<int>dp(n +1 , -1);
+        // int ans =  min(solve( cost, n-1  ,dp) , solve(cost , n -2 , dp));
+        //   return ans;
         int n = cost.size();
-        vector<int>dp(n +1 , -1);
-        int ans =  min(solve( cost, n-1  ,dp) , solve(cost , n -2 , dp));
-          return ans;
+        return solve(cost, n);
     }
 };
